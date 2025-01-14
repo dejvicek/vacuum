adddfe5b27ab3d48f5212375501743fa2b847505
Dejv <buianhviet1995@gmail.com>
2025-01-15 00:35:26 +0100

    docs(swagger): :memo: Setup swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
+
+  const config = new DocumentBuilder()
+    .setTitle('Vacuum API')
+    .setDescription('The Vacuum API description')
+    .setVersion('1.0')
+    .addTag('vacuum')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
