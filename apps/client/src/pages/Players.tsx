576a1267e7ff61b09dc980fec6ea69a1f9069e62
Anh Viet Bui <buianhviet1995@gmail.com>
2025-01-21 03:14:48 +0100

    feat(shadcn): :sparkles: Setup shadcnui

--- apps/client/src/pages/Players.tsx
+++ apps/client/src/pages/Players.tsx
@@ -1,16 +1,48 @@
 import { FC } from "react";
 import { usePlayersQuery } from "../hooks/usePlayersQuery";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export const PlayersPage: FC = () => {
   const { data, isSuccess } = usePlayersQuery();
 
   if (!isSuccess) return "Načítám data hráčů...";
 
-  return data.data?.map(({ first_name, last_name, nick_name }) => (
-    <div>
-      <div>{first_name}</div>
-      <div>{last_name}</div>
-      <div>{nick_name}</div>
-    </div>
-  ));
+  const { data: players } = data;
+
+  return (
+    <Table>
+      <TableCaption>Seznam hráčů.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Přezdívka</TableHead>
+          <TableHead>Jméno</TableHead>
+          <TableHead>Příjmení</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {players?.map(({ first_name, last_name, nick_name }) => (
+          <TableRow key={nick_name}>
+            <TableCell className="font-medium">{nick_name}</TableCell>
+            <TableCell>{first_name}</TableCell>
+            <TableCell>{last_name}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Celkem</TableCell>
+          <TableCell className="text-right">{players?.length} hráčů</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
 };
