056afbf9c9b8224f10aa5b6e41035738df0f4d1f
Dejv <buianhviet1995@gmail.com>
2025-01-24 00:42:17 +0100

    feat: :sparkles: Add pagination to data table

---
+++ apps/client/src/components/data-table-pagination.tsx
@@ -0,0 +1,47 @@
+import { generatePaginationLinks } from "./data-table-pagination-links";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationNext,
+} from "./ui/pagination";
+import { FC } from "react";
+
+interface DataTablePaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (pageNumber: number) => void;
+  showPreviousNext: boolean;
+}
+
+export const DataTablePagination: FC<DataTablePaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+  showPreviousNext,
+}) => {
+  return (
+    <Pagination>
+      <PaginationContent>
+        {showPreviousNext && totalPages ? (
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={() => onPageChange(currentPage - 1)}
+              aria-disabled={currentPage - 1 < 1}
+            />
+          </PaginationItem>
+        ) : null}
+        {generatePaginationLinks(currentPage, totalPages, onPageChange)}
+        {showPreviousNext && totalPages ? (
+          <PaginationItem>
+            <PaginationNext
+              onClick={() => onPageChange(currentPage + 1)}
+              aria-disabled={currentPage > totalPages - 1}
+            />
+          </PaginationItem>
+        ) : null}
+      </PaginationContent>
+    </Pagination>
+  );
+};
--- apps/client/src/components/data-table.tsx
+++ apps/client/src/components/data-table.tsx
@@ -1,12 +1,15 @@
-"use client";
-
 import {
-  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
   flexRender,
   getCoreRowModel,
-  useReactTable,
+  getFilteredRowModel,
   getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
 } from "@tanstack/react-table";
+import * as React from "react";
 
 import {
   Table,
@@ -16,28 +19,56 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTablePagination } from "./data-table-pagination";
 
-import { Button } from "@/components/ui/button";
-
-interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+export interface DataTableProps<TData, TValue> {
   data: TData[];
+  columns: ColumnDef<TData, TValue>[];
 }
 
-export function DataTable<TData, TValue>({
-  columns,
+export const DataTable = <TData, TValue>({
   data,
-}: DataTableProps<TData, TValue>) {
+  columns,
+}: DataTableProps<TData, TValue>) => {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
   const table = useReactTable({
     data,
     columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+    initialState: { pagination: { pageSize: 20 } },
   });
 
   return (
-    <div className="rounded-md border">
-      <div>
+    <div className="w-full">
+      <div className="rounded-md border">
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
@@ -76,35 +107,44 @@ export function DataTable<TData, TValue>({
               ))
             ) : (
               <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
+                <TableCell colSpan={columns.length} className="h-24 flex-auto">
+                  <div className="flex justify-center">No results</div>
                 </TableCell>
               </TableRow>
             )}
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
+      <div className="flex items-center justify-between space-x-2 py-4">
+        <div className="flex items-center space-x-2">
+          <p className="text-sm font-medium">Rows per page</p>
+          <Select
+            value={`${table.getState().pagination.pageSize}`}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value));
+            }}
+          >
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[20, 100, 500].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex justify-end">
+          <DataTablePagination
+            currentPage={table.getState().pagination.pageIndex + 1}
+            totalPages={table.getPageCount()}
+            onPageChange={(pageNumber) => table.setPageIndex(pageNumber - 1)}
+            showPreviousNext
+          />
+        </div>
       </div>
     </div>
   );
-}
+};
